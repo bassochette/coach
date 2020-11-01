de3046f15ddf7b3754f74306e451928ce65b3595
julien prugne <julien@webeleon.dev>
2020-11-01 08:15:17 +0100

    rename default command

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -19,7 +19,7 @@ export class ConfigService {
     this.discordClientId = process.env.DISCORD_CLIENT_ID || '';
     this.mongoURL = process.env.MONGO_URL || 'mongodb://localhost/coach';
     this.adminPrefix = process.env.ADMIN_PREIFX || 'coach admin';
-    this.defaultPrefix = process.env.DEFAULT_PREFIX || '!';
+    this.defaultPrefix = process.env.DEFAULT_PREFIX || 'coach';
   }
 
   getBotInviteLink(permissions = '1075305537'): string {
